de23d75d1faf2565cfb2f2cd815dd9cee87b1f16
luisa-rengifo <82606975+luisa-rengifo@users.noreply.github.com>
2021-05-17 23:31:31 -0400

    Add files via upload

--- src/mapa.js
+++ src/mapa.js
@@ -53,27 +53,27 @@ const info_paises = {
     Afghanistan: {
         psiquiatras: 0.16,
         suicidios: 4.7,
-        Indice: 0
+        indice: 0
     },
     Albania: {
         psiquiatras: 1.83,
-        Suicidios: 6.3,
+        suicidios: 6.3,
         indice: 0
     },
     Algeria: {
         psiquiatras: 1.55,
-        Suicidios: 3.2,
-        Indice: 0
+        suicidios: 3.2,
+        indice: 0
     },
     Andorra: {
         psiquiatras: 6.92,
-        Suicidios: 4.7,
-        Indice: 0
+        suicidios: 4.7,
+        indice: 0
     },
     AntiguayBarbuda: {
         psiquiatras: 1.13,
-        Suicidios: 0.5,
-        Indice: 0
+        suicidios: 0.5,
+        indice: 0
     },
     Armenia: {
         psiquiatras: 4.04,  
@@ -81,107 +81,107 @@ const info_paises = {
         indice: 0
     },
     Australia: {
-        psiquiatra: 12.76,
+        psiquiatras: 12.76,
         suicidios: 13.2,
         indice: 182.37
     },
     Austria: {
-        psiquiatra: 19.71,
+        psiquiatras: 19.71,
         suicidios: 15.6,
         indice: 181.52
     },
     Azerbaijan: {
-        psiquiatra: 5.08,
+        psiquiatras: 5.08,
         suicidios: 2.6,
         indice: 102.88
     },
     Barein: {
-        psiquiatra: 8.18,
+        psiquiatras: 8.18,
         suicidios: 5.9,
         indice: 0
     },
     Banglades: {
-        psiquiatra: 0.07,
+        psiquiatras: 0.07,
         suicidios: 5.9,
         indice: 65.27
     },
     Barbados: {
-        psiquiatra: 4.29,
+        psiquiatras: 4.29,
         suicidios: 0.8,
         indice: 0
     },
     Bielorussia: {
-        psiquiatra: 8.77,
+        psiquiatras: 8.77,
         suicidios: 26.2,
         indice: 0
     },
     Belice: {
-        psiquiatra: 0.96,
+        psiquiatras: 0.96,
         suicidios: 26.2,
         indice: 0
     },
     Benin: {
-        psiquiatra: 0.14,
+        psiquiatras: 0.14,
         suicidios: 9.9,
         indice: 0
     },
     Butan: {
-        psiquiatra: 0.28,
+        psiquiatras: 0.28,
         suicidios: 11.4,
         indice: 0
     },
     Bolivia: {
-        psiquiatra: 1.06,
+        psiquiatras: 1.06,
         suicidios: 12.2,
         indice: 0
     },
     BosniayHerzegovina: {
-        psiquiatra: 4.95,
+        psiquiatras: 4.95,
         suicidios: 8.8,
         indice: 125.71
     },
     Botsuana: {
-        psiquiatra: 0.25,
+        psiquiatras: 0.25,
         suicidios: 9.3,
         indice: 0
     },
     Brasil: {
-        psiquiatra: 3.07,
+        psiquiatras: 3.07,
         suicidios: 6.5,
         indice: 104.75
     },
     Brunei: {
-        psiquiatra: 0.98,
+        psiquiatras: 0.98,
         suicidios: 4.6,
         indice: 0.83
     },
     Bulgaria: {
-        psiquiatra: 6.75,
+        psiquiatras: 6.75,
         suicidios: 4.6,
         indice: 0.83
     },
     Burkina_Faso: {
-        psiquiatra: 0.04,
+        psiquiatras: 0.04,
         suicidios: 7.7,
         indice: 0
     },
     Burundi: {
-        psiquiatra: 0.01,
+        psiquiatras: 0.01,
         suicidios: 9.1,
         indice: 0
     },
     Cabo_verde: {
-        psiquiatra: 1.37,
+        psiquiatras: 1.37,
         suicidios: 11.3,
         indice: 0
     },
     Cambodia: {
-        psiquiatra: 0.23,
+        psiquiatras: 0.23,
         suicidios: 5.3,
         indice: 0
     },
     Camerun: {
-        psiquiatra: 0.03,
+        psiquiatras: 0.03,
         suicidios: 12.2,
         indice: 0
     }
